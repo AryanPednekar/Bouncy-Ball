7678a271ec4a9dc9d0aeb91bbd76919cfd5979d4
AryanPednekar <66258073+AryanPednekar@users.noreply.github.com>
2020-06-08 18:57:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function setup(){
         isStatic: true
     }
     var ball_options ={
-        restitution: 10
+        restitution: 1
     }
     ground = Bodies.rectangle(200,390,200,20,ground_options);
     World.add(world,ground);
